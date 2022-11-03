78622bc38f20318233a05a928c23ad3b83b81eaa
TDOPR <zbowen856@gmail.com>
2022-11-03 11:00:59 +0800

    第一次提交上下级关系表tree_path

--- business/src/main/java/com/haoliang/model/TreePath.java
+++ business/src/main/java/com/haoliang/model/TreePath.java
@@ -1,4 +1,52 @@
 package com.haoliang.model;
 
-public class TreePath {
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@TableName("tree_paths")
+public class TreePath implements Serializable  {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 父
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer ancestor;
+    /**
+     * 子
+     */
+    private Integer descendant;
+    /**
+     * 子是父的几级
+     */
+    private Integer level;
+    /**
+     * 父等级
+     */
+    private Integer aLevel;
+    /**
+     * 子等级
+     */
+    private Integer dLevel;
+    /**
+     * 子给父的返佣
+     */
+    private BigDecimal commission;
+    /**
+     * 是否团队成员 0是 1否
+     */
+    private Integer isGroup;
+
+    /**
+     * 矿机模块等级
+     */
+    @TableField(exist = false)
+    private Integer mineLevel;
 }
